f11ec8b23aff08f85426b7d66af604d56d497268
dforoni <deborah.foroni@gmail.com>
2021-03-08 19:00:24 -0300

    Notas de pesquisa binária.

--- pesquisa_binaria.py
+++ pesquisa_binaria.py
@@ -30,3 +30,11 @@ class PesquisaBinaria():
 
 minha_lista = [1, 3, 5, 7, 9]
 print(pesquisa_binaria(minha_lista, 5))
+
+'''Notas: 
+• A pesquisa binária é muito mais rápida do que a pesquisa simples. 
+• O(log n) é mais rápido do que O(n), e O(log n) fica ainda mais rápido conforme os elementos da lista aumentam. 
+• A rapidez de um algoritmo não é medida em segundos. • O tempo de execução de um algoritmo é medido por meio de seu crescimento. 
+• O tempo de execução dos algoritmos é expresso na notação Big O.
+
+Bhargava, Aditya Y.. Entendendo Algoritmos (p. 43). Novatec Editora. Edição do Kindle. '''
